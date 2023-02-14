e80c06389ef483e3724b537d8d4e009e2f2e99de
Chad <chadjcampbell@gmail.com>
2023-02-14 14:51:23 -0500

    layout Home page and add basic styling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,35 @@
+import styles from "../styles/Home.module.css";
+
 const Home = () => {
-  return <div id="home">Home</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.card}>
+        <article>
+          <section className={styles.top}>
+            <h2>Google Chrome</h2>
+            <p>
+              Google Chrome is a web browser developed by Google, released in
+              2008. Chrome is the world's most popular web browser today!
+            </p>
+          </section>
+          <section className={styles.bottom}>
+            <div className={styles.miniCard}>
+              <img src="./magnifyingGlass.svg" alt="logo" />
+              <h3>Test text</h3>
+            </div>
+            <div className={styles.miniCard}>
+              <img src="./magnifyingGlass.svg" alt="logo" />
+              <h3>Test text</h3>
+            </div>
+            <div className={styles.miniCard}>
+              <img src="./magnifyingGlass.svg" alt="logo" />
+              <h3>Test text</h3>
+            </div>
+          </section>
+        </article>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
