9ccbb5614bb02a8cfe1145e2016d93901b1d0f38
Chad <chadjcampbell@gmail.com>
2023-02-16 18:20:25 -0500

    build and style dropdown

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,35 @@
+import styles from "../styles/Play.module.css";
+
+type DropdownProps = {
+  dropdownDisplay: boolean;
+  dropdownPosition: { x: string; y: string };
+};
+
+export const Dropdown = ({
+  dropdownDisplay,
+  dropdownPosition,
+}: DropdownProps) => {
+  return (
+    <ul
+      style={{
+        display: dropdownDisplay ? "block" : "none",
+        position: "absolute",
+        left: dropdownPosition.x + "px",
+        top: dropdownPosition.y + "px",
+      }}
+      className={styles.dropdown}
+    >
+      <li className={styles.dropdownItem} value="jabba">
+        Jabba
+      </li>
+      <li className={styles.dropdownItem} value="lando">
+        Lando
+      </li>
+      <li className={styles.dropdownItem} value="yoda">
+        Yoda
+      </li>
+    </ul>
+  );
+};
+
+export default Dropdown;
--- src/routes/Play.tsx
+++ src/routes/Play.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Dropdown from "../components/Dropdown";
 import { ImageMagnifier } from "../components/ImageMagnifier";
 import Modal from "../components/Modal";
 import Scoreboard from "../components/Scoreboard";
@@ -8,12 +9,33 @@ const Play = () => {
   const [modalIsOpen, setModalIsOpen] = useState(true);
   const [name, setName] = useState("");
   const [running, setRunning] = useState(false);
+  const [dropdownDisplay, setDropdownDisplay] = useState(false);
   const [modalMessage, setModalMessage] = useState(
     "Enter your name for the leaderboard"
   );
 
+  const [mousePos, setMousePos] = useState({ x: "", y: "" });
+  const [dropdownPosition, setDropdownPosition] = useState({ x: "", y: "" });
+
+  useEffect(() => {
+    const handleMouseMove = (e: { clientX: any; clientY: any }) => {
+      setMousePos({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const handleDropdown = () => {
+    setDropdownDisplay(!dropdownDisplay);
+    setDropdownPosition(mousePos);
+  };
+
   return (
-    <div className={styles.play}>
+    <div onClick={handleDropdown} className={styles.play}>
       <Scoreboard running={running} name={name} />
       {modalIsOpen && (
         <Modal
@@ -24,6 +46,12 @@ const Play = () => {
         />
       )}
       <ImageMagnifier width="100%" src="./sw-wheres-waldo.jpg" />
+      {!modalIsOpen && (
+        <Dropdown
+          dropdownPosition={dropdownPosition}
+          dropdownDisplay={dropdownDisplay}
+        />
+      )}
     </div>
   );
 };
