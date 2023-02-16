0daad7cd9a20ef9c014267ed3e14db8eee94f55d
Chad <chadjcampbell@gmail.com>
2023-02-15 20:03:19 -0500

    add magnifier to image element

--- src/routes/Play.tsx
+++ src/routes/Play.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
+import { ImageMagnifier } from "../components/ImageMagnifier";
 import styles from "../styles/Play.module.css";
 
 const Play = () => {
   return (
     <div className={styles.play}>
-      <img id="waldo" src="./sw-wheres-waldo.jpg" />
+      <ImageMagnifier width="100%" src="./sw-wheres-waldo.jpg" />
     </div>
   );
 };
