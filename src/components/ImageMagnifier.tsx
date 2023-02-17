e4baf68f0dc7d6a6168857f796d61dad420c9f01
Chad <chadjcampbell@gmail.com>
2023-02-17 18:14:21 -0500

    set up image Ref and forward to nested img element

--- src/components/ImageMagnifier.tsx
+++ src/components/ImageMagnifier.tsx
@@ -1,95 +1,98 @@
-import { useState } from "react";
+import { forwardRef, LegacyRef, useState } from "react";
 
 type ImageMagnifierProps = {
   src: string;
-  width?: string;
-  height?: string;
-  magnifierHeight?: number;
-  magnifieWidth?: number;
-  zoomLevel?: number;
+  width: string;
+  height: string;
+  magnifierHeight: number;
+  magnifieWidth: number;
+  zoomLevel: number;
 };
 
-export function ImageMagnifier({
-  src,
-  width,
-  height,
-  magnifierHeight = 100,
-  magnifieWidth = 100,
-  zoomLevel = 1.5,
-}: ImageMagnifierProps) {
-  const [[x, y], setXY] = useState([0, 0]);
-  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
-  const [showMagnifier, setShowMagnifier] = useState(false);
-  return (
-    <div
-      style={{
-        position: "relative",
-        height: height,
-        width: width,
-      }}
-    >
-      <img
-        src={src}
-        style={{
-          height: height,
-          width: width,
-          position: "relative",
-          zIndex: 0,
-        }}
-        onMouseEnter={(e) => {
-          // update image size and turn-on magnifier
-          const elem = e.currentTarget;
-          const { width, height } = elem.getBoundingClientRect();
-          setSize([width, height]);
-          setShowMagnifier(true);
-        }}
-        onMouseMove={(e) => {
-          // update cursor position
-          const elem = e.currentTarget;
-          const { top, left } = elem.getBoundingClientRect();
+const ImageMagnifier = forwardRef(
+  (props: ImageMagnifierProps, ref: LegacyRef<HTMLImageElement>) => {
+    const [[x, y], setXY] = useState([0, 0]);
+    const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+    const [showMagnifier, setShowMagnifier] = useState(false);
 
-          // calculate cursor position on the image
-          const x = e.pageX - left - window.pageXOffset;
-          const y = e.pageY - top - window.pageYOffset;
-          setXY([x, y]);
-        }}
-        onMouseLeave={() => {
-          // close magnifier
-          setShowMagnifier(false);
-        }}
-        alt={"img"}
-      />
-
+    return (
       <div
         style={{
-          display: showMagnifier ? "" : "none",
-          position: "absolute",
-
-          // prevent maginier blocks the mousemove event of img
-          pointerEvents: "none",
-          // set size of magnifier
-          height: `${magnifierHeight}px`,
-          width: `${magnifieWidth}px`,
-          // move element center to cursor pos
-          top: `${y - magnifierHeight / 2}px`,
-          left: `${x - magnifieWidth / 2}px`,
-          opacity: "1", // reduce opacity so you can verify position
-          border: "1px solid lightgray",
-          borderRadius: "50%",
-          backgroundColor: "var(--black)",
-          backgroundImage: `url('${src}')`,
-          backgroundRepeat: "no-repeat",
-
-          //calculate zoomed image size
-          backgroundSize: `${imgWidth * zoomLevel}px ${
-            imgHeight * zoomLevel
-          }px`,
-
-          //calculete position of zoomed image.
-          backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
-          backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+          position: "relative",
+          height: props.height,
+          width: props.width,
         }}
-      ></div>
-    </div>
-  );
-}
+      >
+        <img
+          ref={ref}
+          src={props.src}
+          style={{
+            height: props.height,
+            width: props.width,
+            position: "relative",
+            zIndex: 0,
+          }}
+          onMouseEnter={(e) => {
+            // update image size and turn-on magnifier
+            const elem = e.currentTarget;
+            const { width, height } = elem.getBoundingClientRect();
+            setSize([width, height]);
+            setShowMagnifier(true);
+          }}
+          onMouseMove={(e) => {
+            // update cursor position
+            const elem = e.currentTarget;
+            const { top, left } = elem.getBoundingClientRect();
+
+            // calculate cursor position on the image
+            const x = e.pageX - left - window.pageXOffset;
+            const y = e.pageY - top - window.pageYOffset;
+            setXY([x, y]);
+          }}
+          onMouseLeave={() => {
+            // close magnifier
+            setShowMagnifier(false);
+          }}
+          alt={"img"}
+        />
+
+        <div
+          style={{
+            display: showMagnifier ? "" : "none",
+            position: "absolute",
+
+            // prevent maginier blocks the mousemove event of img
+            pointerEvents: "none",
+            // set size of magnifier
+            height: `${props.magnifierHeight}px`,
+            width: `${props.magnifieWidth}px`,
+            // move element center to cursor pos
+            top: `${y - props.magnifierHeight / 2}px`,
+            left: `${x - props.magnifieWidth / 2}px`,
+            opacity: "1", // reduce opacity so you can verify position
+            border: "1px solid lightgray",
+            borderRadius: "50%",
+            backgroundColor: "var(--black)",
+            backgroundImage: `url('${props.src}')`,
+            backgroundRepeat: "no-repeat",
+
+            //calculate zoomed image size
+            backgroundSize: `${imgWidth * props.zoomLevel}px ${
+              imgHeight * props.zoomLevel
+            }px`,
+
+            //calculete position of zoomed image.
+            backgroundPositionX: `${
+              -x * props.zoomLevel + props.magnifieWidth / 2
+            }px`,
+            backgroundPositionY: `${
+              -y * props.zoomLevel + props.magnifierHeight / 2
+            }px`,
+          }}
+        ></div>
+      </div>
+    );
+  }
+);
+
+export default ImageMagnifier;
--- src/routes/Play.tsx
+++ src/routes/Play.tsx
@@ -1,6 +1,6 @@
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useRef, useState } from "react";
 import Dropdown from "../components/Dropdown";
-import { ImageMagnifier } from "../components/ImageMagnifier";
+import ImageMagnifier from "../components/ImageMagnifier";
 import Modal from "../components/Modal";
 import Scoreboard from "../components/Scoreboard";
 import styles from "../styles/Play.module.css";
@@ -10,10 +10,11 @@ const Play = () => {
   const [name, setName] = useState("");
   const [running, setRunning] = useState(false);
   const [dropdownDisplay, setDropdownDisplay] = useState(false);
+  const [dropdownPosition, setDropdownPosition] = useState({ x: 0, y: 0 });
   const [modalMessage, setModalMessage] = useState(
     "Enter your name for the leaderboard"
   );
-  const [dropdownPosition, setDropdownPosition] = useState({ x: 0, y: 0 });
+  const imageRef = useRef<HTMLImageElement>(null);
 
   const handleDropdown = (e: MouseEvent) => {
     if (!modalIsOpen && !dropdownDisplay) {
@@ -24,6 +25,9 @@ const Play = () => {
       const y = e.pageY - top - window.pageYOffset;
       setDropdownDisplay(true);
       setDropdownPosition({ x: x, y: y });
+      console.log(dropdownPosition);
+      console.log(imageRef.current?.offsetHeight);
+      console.log(imageRef.current?.offsetWidth);
     } else {
       setDropdownDisplay(false);
     }
@@ -31,6 +35,12 @@ const Play = () => {
 
   return (
     <div onClick={(e) => handleDropdown(e)} className={styles.play}>
+      {/*TODO - use these + imageRef to find responsive value of characters */}
+      <div style={{ position: "absolute", zIndex: "5" }}>
+        <div style={{ color: "white" }}>o</div>
+        <div style={{ color: "white" }}>o</div>
+        <div style={{ color: "white" }}>o</div>
+      </div>
       <Scoreboard running={running} name={name} />
       {modalIsOpen && (
         <Modal
@@ -40,7 +50,15 @@ const Play = () => {
           modalMessage={modalMessage}
         />
       )}
-      <ImageMagnifier width="100%" src="./sw-wheres-waldo.jpg" />
+      <ImageMagnifier
+        ref={imageRef}
+        height="auto"
+        magnifierHeight={100}
+        magnifieWidth={100}
+        zoomLevel={1.5}
+        width="100%"
+        src="./sw-wheres-waldo.jpg"
+      />
       <Dropdown
         dropdownPosition={dropdownPosition}
         dropdownDisplay={dropdownDisplay}
