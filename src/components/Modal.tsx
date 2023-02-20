af4e8fa194fff657ce25a615cae4c890b6e54ac1
Chad <chadjcampbell@gmail.com>
2023-02-20 16:18:21 -0500

    update Modal to allow anonymous play, update db

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,11 @@
-import { FormEvent, SyntheticEvent } from "react";
+import { FormEvent, SyntheticEvent, useEffect, useState } from "react";
 import styles from "../styles/Play.module.css";
 import {
-  collection,
-  addDoc,
   arrayUnion,
   doc,
-  Timestamp,
   updateDoc,
+  increment,
+  onSnapshot,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
@@ -29,56 +28,89 @@ const Modal = ({
   totalTime,
 }: ModalProps) => {
   const navigate = useNavigate();
+  const [anon, setAnon] = useState("Anonymous#");
+  const [loading, setLoading] = useState(true);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
     setModalIsOpen(false);
     const form = e.target as HTMLFormElement;
-    setName(form.scoreboardName.value);
+    if (form.scoreboardName.value === "") {
+      setName(anon);
+    } else {
+      setName(form.scoreboardName.value);
+    }
     setRunning(true);
   };
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "myApp", "leaderboard"), (doc) => {
+      const data = doc?.data()?.anon;
+      setAnon((anon) => anon + data);
+      setLoading(false);
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+
   const updateLeaderboard = async () => {
     await updateDoc(doc(db, "myApp", "leaderboard"), {
       leaders: arrayUnion({
         name,
         totalTime,
       }),
-    }).then(() => {
-      navigate("/leaderboard");
-    });
+    })
+      .then(async () => {
+        if (anon !== "Anonymous#") {
+          await updateDoc(doc(db, "myApp", "leaderboard"), {
+            anon: increment(1),
+          });
+        }
+      })
+      .finally(() => {
+        navigate("/leaderboard");
+      });
   };
 
   return (
     <div className={styles.modal}>
       <div className={styles.modalContent}>
-        <h2>{modalMessage}</h2>
-        {totalTime !== "00:00" && (
+        {loading ? (
+          <h2>loading...</h2>
+        ) : (
           <>
-            <h2>Name: {name}</h2>
-            <h2>Time: {totalTime}</h2>
-            <button onClick={updateLeaderboard} className={styles.modalBtn}>
-              Submit to Leaderboard
-            </button>
-          </>
-        )}
-        {totalTime === "00:00" && (
-          <form onSubmit={(e: FormEvent<HTMLFormElement>) => handleSubmit(e)}>
-            <input
-              autoComplete="off"
-              autoFocus
-              className={styles.modalInput}
-              type="text"
-              minLength={2}
-              maxLength={20}
-              placeholder="Enter Name"
-              required
-              id="scoreboardName"
-            />
+            <h2>{modalMessage}</h2>
+            {totalTime !== "00:00" && (
+              <>
+                <h2>Name: {name}</h2>
+                <h2>Time: {totalTime}</h2>
+                <button onClick={updateLeaderboard} className={styles.modalBtn}>
+                  Submit to Leaderboard
+                </button>
+              </>
+            )}
+            {totalTime === "00:00" && (
+              <form
+                onSubmit={(e: FormEvent<HTMLFormElement>) => handleSubmit(e)}
+              >
+                <input
+                  autoComplete="off"
+                  autoFocus
+                  className={styles.modalInput}
+                  type="text"
+                  minLength={2}
+                  maxLength={20}
+                  placeholder={anon}
+                  id="scoreboardName"
+                />
 
-            <button type="submit" className={styles.modalBtn}>
-              Play!
-            </button>
-          </form>
+                <button type="submit" className={styles.modalBtn}>
+                  Play!
+                </button>
+              </form>
+            )}
+          </>
         )}
       </div>
     </div>
