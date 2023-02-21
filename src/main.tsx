6fa8d66b39783f6bd9056f7307d1a7746d369c74
Chad <chadjcampbell@gmail.com>
2023-02-21 17:20:13 -0500

    prepare for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,20 @@ import Leaderboard from "./routes/Leaderboard";
 import Play from "./routes/Play";
 import Root from "./routes/Root";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/play", element: <Play /> },
-      { path: "/leaderboard", element: <Leaderboard /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/play", element: <Play /> },
+        { path: "/leaderboard", element: <Leaderboard /> },
+      ],
+    },
+  ],
+  { basename: "/scavenger" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
